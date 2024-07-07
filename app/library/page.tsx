77a2a0cf7f513c20c7904f01885002c0b20f8093
Diogo Ruivo <diogocaruivo@gmail.com>
2024-07-07 21:00:14 +0200

    Feat: Library Page

---
+++ app/library/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Page() {
+    return (
+        <main className='flex flex-col justify-center'>
+        <h2 className='text-center'>Welcome to my Library Page!</h2>
+        <p className='text-center'>Here you can find some of my favourite books.</p>
+        </main>
+    );
+}
